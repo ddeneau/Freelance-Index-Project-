c40a625f976aaf19d2a176f1539fb2935e625d41
Danny <ddeneau@terpmail.umd.edu>
2023-10-27 22:34:50 -0400

    Sorted out all of the navigation quirks.
    It turns out you can't wrap a <Link/> in <div></div> tags and have the links work. So I replaced the divs with links and kept the same tailwind class name to keep the exact same style.

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,24 +5,16 @@ const NavBar = () => {
         <nav>
             <ul class="pt-6 pr-10 pl-10 flex">
                 <li class="flex-1 mr-2">
-                    <div class="text-center block border border-black rounded hover:border-gray-200 text-black-500 font-semibold text-xl hover:bg-black hover:text-white py-2 px-4">
-                        <Link to="/">Home</Link>
-                    </div>
+                    <Link class="text-center block border border-black rounded hover:border-gray-200 text-black-500 font-semibold text-xl hover:bg-black hover:text-white py-2 px-4" to="/">Home</Link>
                 </li>
                 <li class="flex-1 mr-2">
-                    <div class="text-center block border border-black rounded hover:border-gray-200 text-black-500 font-semibold text-xl hover:bg-black hover:text-white py-2 px-4">
-                        <Link to="/board">Job Board</Link>
-                    </div>
+                    <Link class="text-center block border border-black rounded hover:border-gray-200 text-black-500 font-semibold text-xl hover:bg-black hover:text-white py-2 px-4" to="/board">Job Board</Link>
+                </li>
+                <li class="flex-1 mr-2">   
+                    <Link class="text-center block border border-black rounded hover:border-gray-200 text-black-500 font-semibold text-xl hover:bg-black hover:text-white py-2 px-4" to="/social">Social</Link>
                 </li>
                 <li class="flex-1 mr-2">
-                    <div class="text-center block border border-black rounded hover:border-gray-200 text-black-500 font-semibold text-xl hover:bg-black hover:text-white py-2 px-4">
-                        <Link to="/social">Social</Link>
-                    </div>
-                </li>
-                <li class="flex-1 mr-2">
-                    <div class="text-center block border border-black rounded hover:border-gray-200 text-black-500 font-semibold text-xl hover:bg-black hover:text-white py-2 px-4">
-                        <Link to="/profile">Your Profile</Link>
-                    </div>
+                    <Link class="text-center block border border-black rounded hover:border-gray-200 text-black-500 font-semibold text-xl hover:bg-black hover:text-white py-2 px-4" to="/profile">Your Profile</Link>
                 </li>
             </ul>
         </nav>
