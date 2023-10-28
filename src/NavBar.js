a31588e881178e27f68eaadf59d1e4cac07540ef
Danny <ddeneau@terpmail.umd.edu>
2023-10-28 02:03:39 -0400

    Minor changes to front-end design for mobile/desktop responsiveness.
    Padding adjustment using Breakpoints in the nav bar and in the home screen

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <nav>
-            <ul class="pt-6 pr-10 pl-10 flex">
+        <nav class="justify-items-center">
+            <ul class="pt-6 lg:px-10 sm:px-5 flex">
                 <li class="flex-1 mr-2">
                     <Link class="text-center block border border-black rounded hover:border-gray-200 text-black-500 font-semibold text-xl hover:bg-black hover:text-white py-2 px-4" to="/">Home</Link>
                 </li>
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -2,16 +2,16 @@
 
 const HomeScreen = () => {
     return (
-        <div className="block border border-black rounded my-20 mx-20">
+        <div className="block border border-black rounded my-20 lg:mx-20 justify-self-center">
             <div className="flex items-center justify-center font-extrabold text-4xl">
        
             </div>
             <div className="grid lg:grid-cols-2 sm:grid-cols-1" >
                 <div>
-                    <div className="flex items-center justify-center text-4xl pt-3 px-11">
+                    <div className="flex items-center justify-center text-4xl pt-3 lg:px-11">
                         Create and Post Contracts.
                     </div>
-                    <div className="flex grow text-2xl px-11 py-11 font-light text-center leading-loose">
+                    <div className="flex grow text-2xl lg:px-11 py-11 font-light text-center leading-loose">
                     Create and customize job listings quickly. You can specify job requirements, responsibilities, and any other details important to you. 
                     Connect you with the most suitable candidates, streamlining the hiring process.
                     </div>
@@ -20,7 +20,7 @@ const HomeScreen = () => {
                     <div className="flex items-center justify-center text-4xl pt-3 px-11">
                         Browse Available Contracts.
                     </div>
-                    <div className="flex grow text-2xl px-11 py-11 font-light text-center leading-loose">
+                    <div className="flex grow text-2xl lg:px-11 py-11 font-light text-center leading-loose">
                     Browse through a diverse range of job listings with ease. 
                     Whether you're seeking part-time, full-time, remote, or on-site positions, 
                     we've got you covered. Search filters help you find the jobs that align perfectly with your qualifications and personal needs.
@@ -28,23 +28,12 @@ const HomeScreen = () => {
                 </div>
             </div>
             <div className="flex items-center justify-center font-extrabold text-4xl pt-10">
-            <div className="flex flex-row">
-                <img
-                    src="https://cdn-icons-png.flaticon.com/512/5434/5434690.png"
-                    class="resize h-auto max-w-xs object-cover mx-6"
-                    alt="" />
+                <div className="flex flex-row">
                     <img
-                    src="https://cdn-icons-png.flaticon.com/512/5434/1077077.png"
-                    class="resize lg:h-auto lg:max-w-xs md:min-w-ws object-cover mx-6"
-                    alt="" />
-                    <img
-                    src="https://cdn-icons-png.flaticon.com/512/5434/5434690.png"
-                    class="resize lg:h-auto lg:max-w-xs md:min-w-ws object-cover mx-6"
-                    alt="" />
-                    <img
-                    src="https://cdn-icons-png.flaticon.com/512/5434/5434690.png"
-                    class="resize lg:h-auto lg:max-w-xs md:min-w-ws object-cover mx-6"
-                    alt="" />
+                        src="https://cdn-icons-png.flaticon.com/512/5434/5434690.png"
+                        class="resize h-auto max-w-xs object-cover mx-6"
+                        alt=""
+                    />
                 </div>
             </div>
             <div className="grid lg:grid-cols-2 sm:grid-cols-1" >
