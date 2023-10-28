0aa895a282eeabf8ba12a2deaae5203e252422ad
Danny <ddeneau@terpmail.umd.edu>
2023-10-27 23:59:27 -0400

    Began adding responsive design to classes

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -6,21 +6,21 @@ const HomeScreen = () => {
             <div className="flex items-center justify-center font-extrabold text-4xl">
        
             </div>
-            <div className="grid grid-cols-2" >
+            <div className="grid lg:grid-cols-2 sm:grid-cols-1" >
                 <div>
                     <div className="flex items-center justify-center text-4xl pt-3 px-11">
                         Create and Post Contracts.
                     </div>
-                    <div className="text-2xl px-11 py-11 font-light text-center leading-loose">
+                    <div className="flex grow text-2xl px-11 py-11 font-light text-center leading-loose">
                     Create and customize job listings quickly. You can specify job requirements, responsibilities, and any other details important to you. 
                     Connect you with the most suitable candidates, streamlining the hiring process.
                     </div>
                 </div>
                 <div>
-                    <div className="flex items-center justify-center text-4xl pt-3 px-11">
+                    <div className="flex items-center justify-center lg:text-4xl pt-3 px-11">
                         Browse Available Contracts.
                     </div>
-                    <div className="text-2xl px-11 py-11 font-light text-center leading-loose">
+                    <div className="flex grow text-2xl px-11 py-11 font-light text-center leading-loose">
                     Browse through a diverse range of job listings with ease. 
                     Whether you're seeking part-time, full-time, remote, or on-site positions, 
                     we've got you covered. Search filters help you find the jobs that align perfectly with your qualifications and personal needs.
@@ -31,23 +31,23 @@ const HomeScreen = () => {
             <div className="flex flex-row">
                 <img
                     src="https://cdn-icons-png.flaticon.com/512/5434/5434690.png"
-                    class="h-auto max-w-xs object-cover mx-6"
+                    class="resize lg:h-auto lg:max-w-xs md:min-w-ws object-cover mx-6"
                     alt="" />
                     <img
                     src="https://cdn-icons-png.flaticon.com/512/5434/1077077.png"
-                    class="h-auto max-w-xs object-cover mx-6"
+                    class="resize lg:h-auto lg:max-w-xs md:min-w-ws object-cover mx-6"
                     alt="" />
                     <img
                     src="https://cdn-icons-png.flaticon.com/512/5434/5434690.png"
-                    class="h-auto max-w-xs object-cover mx-6"
+                    class="resize lg:h-auto lg:max-w-xs md:min-w-ws object-cover mx-6"
                     alt="" />
                     <img
                     src="https://cdn-icons-png.flaticon.com/512/5434/5434690.png"
-                    class="h-auto max-w-xs object-cover mx-6"
+                    class="resize lg:h-auto lg:max-w-xs md:min-w-ws object-cover mx-6"
                     alt="" />
                 </div>
             </div>
-            <div className="grid grid-cols-2" >
+            <div className="grid lg:grid-cols-2 sm:grid-cols-1" >
                 <div>
                     <div className="flex items-center justify-center text-4xl pt-3 px-11">
                     Create and Post A Profile.
